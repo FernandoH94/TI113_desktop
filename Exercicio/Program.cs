4f24c8a152e4a06c48418805a1f7c59430b9a21e
FernandoH94 <fernando.hsousa@senacsp.edu.br>
2025-02-18 19:54:20 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digitar a primeira nota");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
