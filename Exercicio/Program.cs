247a2438c8736013310d53dbf6030d43b88c44a3
FernandoH94 <fernando.hsousa@senacsp.edu.br>
2025-02-18 20:01:32 -0300

    feat: ex1 feito

--- Exercicio/Program.cs
+++ Exercicio/Program.cs
@@ -4,8 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Digitar a primeira nota");
+            Console.Write("Digitar a primeira nota: ");
             double nota1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Digitar a primeira nota: ");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            double media = (nota1 + nota2) / 2;
+
+            Console.WriteLine($"A média é: {media:F2}");
+
+            if(media >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else if (media >= 5)
+            {
+                Console.WriteLine("Recuperação");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+
         }
     }
 }
