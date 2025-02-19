18c45906f6246a4f2c25780227638f13a3d6825f
FernandoH94 <fernando.hsousa@senacsp.edu.br>
2025-02-19 19:33:12 -0300

    feat: ex 6 feito

---
+++ Exercicio 6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double valorHora;
+            short quantHora;
+
+            Console.Write("Digite o seu Nome: ");
+            nome = Console.ReadLine()!;
+
+            Console.Write("Digite o núemero de horas trabalhadas: ");
+            quantHora = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Digite o valor por hora");
+            valorHora = Convert.ToDouble(Console.ReadLine());
+
+            Double total = valorHora * quantHora;
+
+            Console.WriteLine($@"
+                    O Valor total {nome} é R$ {total:f2}");
+
+        }
+    }
+}
