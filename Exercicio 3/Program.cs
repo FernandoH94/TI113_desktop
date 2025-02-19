58451ad28a67a5b1554467bb13d5fcd3f2a16737
FernandoH94 <fernando.hsousa@senacsp.edu.br>
2025-02-18 21:21:41 -0300

    feat: ex3 feito

---
+++ Exercicio 3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um 1º Número: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            Console.Write("Digite o 2º Número: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            Console.Write($"O maior número é {Math.Max(numero1, numero2)}");
+
+        }
+    }
+}
